24cd204a9c6b14395be565601bec1927ea444845
JQB <jordanqbehn@gmail.com>
2021-03-15 20:44:28 -0500

    created about component

---
+++ src/components/About/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+export default function About(){
+    return (
+      <div class='main'>
+
+        <h2> about: </h2>
+        <p>Hi, I'm Jordan, a fullstack web developer. </p>
+      </div>
+    );
+}
