5a8df058b8f2a764526f0587c5b1058ff48990b6
JQB <jordanqbehn@gmail.com>
2021-03-15 20:45:45 -0500

    added contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,18 @@
+import './JordanBehnResume.pdf'
+export default function Contact() {
+  return (
+    <cardbody class="card-body">
+      <h3 class="card-title">contact information:</h3>
+      <p class="card-text">Jordan.Q.Behn@gmail.com</p>
+      <p></p>
+      <a href="https://github.com/JordanBehn ">Github </a>
+      <a href="https://www.linkedin.com/in/jordan-behn-3a6ba3101/ ">
+        Linkedin{" "}
+      </a>
+      <a href="./JordanBehnResume.pdf" download="Resume">
+      Résumé
+      </a>
+      <br></br>
+    </cardbody>
+  );
+}
